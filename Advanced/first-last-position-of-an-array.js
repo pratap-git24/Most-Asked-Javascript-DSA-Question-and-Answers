753f5907198d1f814bba2454a141bda56c5a0216
Pratap Git <pratapkumarnayak09@gmail.com>
2024-09-12 20:53:43 +0530

    optimize code

--- Advanced/first-last-position-of-an-array.js
+++ Advanced/first-last-position-of-an-array.js
@@ -1,35 +1,18 @@
 //Find First and Last Position of an Element in a Sorted Array
+function searchRangeUnsorted(arr, target) {
+    let first = -1;
+    let last = -1;
 
-function searchRange(arr, target) {
-    const findPosition = (arr, target, findFirst) => {
-        let left = 0;
-        let right = arr.length - 1;
-        let position = -1;
-
-        while (left <= right) {
-            const mid = Math.floor((left + right) / 2);
-            if (arr[mid] === target) {
-                position = mid;
-                if (findFirst) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            if (first === -1) {
+                first = i;  // Set first occurrence
             }
+            last = i;  // Update last occurrence
         }
+    }
 
-        return position;
-    };
-
-    const first = findPosition(arr, target, true);
-    const last = findPosition(arr, target, false);
-    
     return [first, last];
 }
 
-console.log(searchRange([1,2,3,4],2));
-
+console.log(searchRangeUnsorted([1, 2, 3, 4], 2));
