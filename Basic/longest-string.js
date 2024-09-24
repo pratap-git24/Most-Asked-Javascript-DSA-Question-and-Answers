c490bb673b4cb202b6cbaa4351cabe14e0ec4611
Pratap Git <pratapkumarnayak09@gmail.com>
2024-09-24 18:13:39 +0530

    longest-array

---
+++ Basic/longest-string.js
@@ -0,0 +1,22 @@
+// find the longest string from an array
+
+// solution 1
+function longestArr(str){
+    let words = str.split(" ");
+    let word = words.sort((a,b)=>b.length-a.length)
+    return word[0];
+}
+
+//console.log(longestArr("My name is pratap"));
+
+// solution 2
+function longestArr2(str){
+    let words = str.split(" ");
+    let longestWord = words.reduce((longest,currWord)=>{
+        //return longest.length > currWord.length ? longest : currWord
+        return currWord.length < longest.length ? longest : currWord;
+    },'')
+    return longestWord;
+}
+
+console.log(longestArr2("My99999 name is laxman"));
