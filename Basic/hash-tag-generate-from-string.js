f5d6e9ca77bd65625be55ab0659c0c2268c025bf
Pratap Git <pratapkumarnayak09@gmail.com>
2024-09-27 18:14:22 +0530

    hash tag generater

---
+++ Basic/hash-tag-generate-from-string.js
@@ -0,0 +1,12 @@
+// Generate Hash Tag from String
+
+function generateHash(str){
+    let words = str.split(" ");
+    let eachStr = words.map((word)=> word.replace(word[0],word[0].toUpperCase()))
+    return `#${eachStr.join("")}`
+    console.log(eachStr);
+    
+
+}
+
+console.log(generateHash("javascript dsa question"));
